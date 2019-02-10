9f29d7376286d0c108935ad21c66e5644f5a2ad7
x-nobu <n13singhz@live.ca>
2019-02-10 17:40:16 -0500

    Lecture 126

--- Assets/Scenes/Scripts/EnemyMovement.cs
+++ Assets/Scenes/Scripts/EnemyMovement.cs
@@ -5,22 +5,23 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
 
-    [SerializeField] List<Block> path;
+    [SerializeField] List<Waypoint> path;
 	// Use this for initialization
 	void Start () {
-        PrintAllWaypoints();
+       StartCoroutine(FollowPath());
 	}
 
-    private void PrintAllWaypoints()
+    IEnumerator FollowPath()
     {
-        foreach (Block waypoint in path)
+        print("Starting patrol");
+        foreach (Waypoint waypoint in path)
         {
-            print(waypoint.name);
+            transform.position = waypoint.transform.position;
+            print("Visitng " + waypoint);
+            yield return new WaitForSeconds(1f);
         }
+        print("Ending Patrol");
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+
 }
