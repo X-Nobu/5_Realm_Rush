7e356261bbe31d3f0567f95176e499e375eef5c6
x-nobu <n13singhz@live.ca>
2019-02-10 13:28:57 -0500

    Lecture 124

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour {
+
+    //puts a field in the cube game object that makes gridsize 10 by default and can be adjusted between range 1f-20f
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+    void Update()
+    {
+        Vector3 snapPos;
+        // example postion 16/gridsize rounds to 2 mulitplied by gridsize give amount of snapping
+        snapPos.x = Mathf.RoundToInt(transform.position.x/gridSize) * gridSize;
+
+        snapPos.z = Mathf.RoundToInt(transform.position.z/gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+
+}
