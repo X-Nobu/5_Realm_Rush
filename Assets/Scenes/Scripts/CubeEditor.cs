99dedc72833830f404a43f6c7f24b9cd183abc23
x-nobu <n13singhz@live.ca>
2019-02-10 14:10:03 -0500

    Lecture 124

--- Assets/Scenes/Scripts/CubeEditor.cs
+++ Assets/Scenes/Scripts/CubeEditor.cs
@@ -3,19 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour {
+[SelectionBase]
+public class CubeEditor : MonoBehaviour {
 
     //puts a field in the cube game object that makes gridsize 10 by default and can be adjusted between range 1f-20f
     [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+    
+
+    TextMesh textMesh;
+
+    void Start()
+    {
+      
+    }
+
     void Update()
     {
         Vector3 snapPos;
-        // example postion 16/gridsize rounds to 2 mulitplied by gridsize give amount of snapping
+        
+        // example postion 16/10 rounds to 2 mulitplied by 10 give amount of snapping 20
         snapPos.x = Mathf.RoundToInt(transform.position.x/gridSize) * gridSize;
-
         snapPos.z = Mathf.RoundToInt(transform.position.z/gridSize) * gridSize;
-
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+
+        
     }
 
 }
