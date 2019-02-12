2ed9cc40c8c0d9ad176940e29bbfc48cc75593e2
x-nobu <n13singhz@live.ca>
2019-02-11 22:33:00 -0500

    Lecture 150

--- Assets/Scenes/Scripts/Waypoint.cs
+++ Assets/Scenes/Scripts/Waypoint.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Waypoint : MonoBehaviour {
-    
+
+   
     public bool isExplored = false;
     public Waypoint exploredFrom;
     public bool isPlaceable = true;
+
+    [SerializeField] Tower towerPrefab;
+
     Vector2Int gridPos;
     const int gridSize = 10;
-    
+
    
-	public int GetGridSize()
+
+    public int GetGridSize()
     {
         return gridSize;
     }
@@ -30,7 +35,7 @@ public class Waypoint : MonoBehaviour {
         {
             if (isPlaceable)
             {
-                print(gameObject.name);
+                Instantiate(towerPrefab, transform.position, Quaternion.identity);
             }
             else
             {
