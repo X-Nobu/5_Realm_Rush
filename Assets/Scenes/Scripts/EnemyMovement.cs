628c839b37eb688f55e501ce3b8fd164cded7b86
x-nobu <n13singhz@live.ca>
2019-02-12 11:18:16 -0500

    Lecture 156

--- Assets/Scenes/Scripts/EnemyMovement.cs
+++ Assets/Scenes/Scripts/EnemyMovement.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
 
-    
+    [SerializeField] float movementPeriod = .5f;
+    [SerializeField] ParticleSystem goalParticle;
+
 	// Use this for initialization
 	void Start ()
     {
@@ -20,10 +22,16 @@ public class EnemyMovement : MonoBehaviour {
         foreach (Waypoint waypoint in path)
         {
             transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(movementPeriod);
         }
-        print("Ending Patrol");
-    } 
+        SelfDestruct();
+    }
 
-
+    private void SelfDestruct()
+    {
+        var vfx = Instantiate(goalParticle, transform.position, Quaternion.identity);
+        vfx.Play();
+        Destroy(vfx.gameObject, vfx.main.duration);
+        Destroy(gameObject);
+    }
 }
